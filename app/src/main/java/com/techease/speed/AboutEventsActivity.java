9a26b1171689db4190f4d32aa78679ca80ae260e
attaurrahman <attaurrahman321@gmail.com>
2019-03-11 17:35:02 +0500

    some screen design done

--- app/src/main/java/com/techease/speed/AboutEventsActivity.java
+++ app/src/main/java/com/techease/speed/AboutEventsActivity.java
@@ -1,17 +1,14 @@
 package com.techease.speed;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class AboutEventsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_events);
 
-        startActivity(new Intent(this, ProfileActivity.class));
     }
 }
